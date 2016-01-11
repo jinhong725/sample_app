a12c0538c4237b98590d07680d24644ea861ed46
YOUR NAME <YOUR@EMAIL.com>
2016-01-11 03:26:25 +0100

    env s3

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,9 +3,10 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => 'AKIAIZ2ZVC37QRWURFIQ',
-      :aws_secret_access_key => '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'
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY'],
+      :region => ENV['S3_REGION']
     }
-    config.fog_directory     =  'volleyballbucket'
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
